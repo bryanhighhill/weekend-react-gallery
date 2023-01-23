d4d5a8e3013f3059f4f18296e5d89f347649ca9f
Bryan Highhill <bryanhighhill@gmail.com>
2023-01-23 12:06:42 -0600

    added css styling to ImageForm and added a button to show/hide form

--- src/components/ImageForm/ImageForm.jsx
+++ src/components/ImageForm/ImageForm.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import './ImageForm.css';
 
 const ImageForm = ({postImage}) => {
     const [url, setUrl] = useState('');
     const [description, setDescription] = useState('');
+    const [formVisible, setFormVisible] = useState(false);
 
     const onSubmit = (event) => {
         event.preventDefault();
@@ -11,35 +13,47 @@ const ImageForm = ({postImage}) => {
 
     return (
         <>
-            <h2>Add Image</h2>
-            {/* post request function called here */}
-            <form onSubmit={onSubmit}>
+            <button 
+                className="form-visible-button"
+                onClick={() => {setFormVisible(!formVisible)}}
+            >
+                Add Image Form
+            </button>
+            {formVisible && <div className="image-form">
+                <br />
+                {/* post request function called here */}
+                <form onSubmit={onSubmit}>
 
-                {/* form needs 2 inputs: URL of image, description of image */}
-                <label htmlFor="url">URL: </label>
+                    {/* form needs 2 inputs: URL of image, description of image */}
+                    <label htmlFor="url">URL: </label>
 
-                <input 
-                    id="url" 
-                    //name attribute should match html attribute
-                    name="url"
-                    value={url} 
-                    placeholder="image url"
-                    onChange={(event) => setUrl(event.target.value)}
-                />
+                    <input 
+                        id="url" 
+                        //name attribute should match html attribute
+                        name="url"
+                        value={url} 
+                        placeholder="image url"
+                        onChange={(event) => setUrl(event.target.value)}
+                    />
+                    &nbsp; &nbsp; &nbsp; &nbsp; 
+                    <label htmlFor="description">Description: </label>
 
-                <label htmlFor="description">Description: </label>
-
-                <input 
-                    id="description" 
-                    //name attribute should match html attribute
-                    name="description"
-                    value={description} 
-                    placeholder="image description"
-                    onChange={(event) => setDescription(event.target.value)}
-                />
-
-                <button type="submit">Add New Image</button>
-            </form>
+                    <input 
+                        id="description" 
+                        //name attribute should match html attribute
+                        name="description"
+                        value={description} 
+                        placeholder="image description"
+                        onChange={(event) => setDescription(event.target.value)}
+                    /> 
+                    &nbsp; &nbsp; &nbsp; &nbsp; 
+                    <button 
+                        type="submit"
+                        className="submit-image-button"
+                    >
+                        Submit Image</button>
+                </form>
+            </div>}
         </>
     )
 }
